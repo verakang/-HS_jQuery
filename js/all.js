e0aacd2cbb6574c76585937e5a6cb3cbb2eab5be
Vera <sk70885@hotmail.com>
2022-05-21 20:12:09 +0800

    first commit

---
+++ js/all.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    // 下拉式選單功能
+    $('.menu li h3').click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $(this).parent().find('ul').slideToggle();
+        $(this).parent().siblings().find('ul').slideUp();
+        $(this).parent().siblings().find('h3').removeClass('active');
+    });
+
+    // scroll to top 效果
+    $('.goback a').click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    // Lightbox 燈箱效果
+
+
+});
